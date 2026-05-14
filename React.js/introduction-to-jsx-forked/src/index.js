86c625c52eefe29169cefcb6d7f01cb31ef164d6
David Aning Amponsah <amponsahaning@gmail.com>
2026-05-14 11:57:57 -0700

    Add files via upload

---
+++ React.js/introduction-to-jsx-forked/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>welcome home everyone</p>
+  </div>,
+  document.querySelector("#root")
+);
