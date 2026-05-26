c442904244f9671f49c23d3113e1ef8240485d94
David Aning Amponsah <amponsahaning@gmail.com>
2026-05-26 12:07:25 -0700

    Add files via upload

---
+++ keeper-app-part-1-starting-forked/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Note from "./Note.jsx";
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
