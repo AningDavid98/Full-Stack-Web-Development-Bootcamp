4a2ca77c5622756d88d1a14b2394ddac04292ac8
David Aning Amponsah <amponsahaning@gmail.com>
2026-07-04 08:20:18 -0700

    Add files via upload

---
+++ event-handling-in-react-forked/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function App() {
+  const [heading, setHeading] = useState("Hello");
+
+  function handleClick() {
+    setHeading("Submitted");
+  }
+
+  const [mouseOver, setMouseOver] = useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{heading}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: mouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
